fd10c504ee90790053b1a082beb6eae1979e5ba4
Lisgevan <episimo@hotmail.com>
2021-10-12 11:04:15 +0300

    reusable card component

---
+++ projfrontend/src/core/Card.js
@@ -0,0 +1,68 @@
+import React from "react";
+import ImageHelper from "./helper/ImageHelper";
+import { Redirect } from "react-router-dom";
+
+//TODO: Deal with this later
+const isAuhenticated = false;
+
+const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
+	const cartTitle = product ? product.name : "A photo from pexels";
+	const cartDescription = product ? product.description : "Default description";
+	const cartPrice = product ? product.price : "Default";
+
+	const addToCart = () => {
+		if (isAuhenticated) {
+			console.log("Added to cart");
+		} else {
+			console.log("Login Please!");
+		}
+	};
+
+	const getAredirect = redirect => {
+		if (redirect) {
+			return <Redirect to="/cart" />;
+		}
+	};
+
+	const showAddToCart = addToCart => {
+		return (
+			addToCart && (
+				<button onClick={addToCart} className="btn btn-block btn-outline-success mt-2 mb-2">
+					Add to Cart
+				</button>
+			)
+		);
+	};
+
+	const showRemoveFromCart = addToCart => {
+		return (
+			addToCart && (
+				<button
+					onClick={() => {
+						//TODO: handle this too
+						console.log("Product removed from cart");
+					}}
+					className="btn btn-block btn-outline-danger mt-2 mb-2">
+					Remove from cart
+				</button>
+			)
+		);
+	};
+
+	return (
+		<div className="card text-white bg-transparent border border-info ">
+			<div className="card-header lead">{cartTitle}</div>
+			<div className="card-body">
+				<ImageHelper product={product} />
+				<p className="lead bg-success font-weight-normal text-wrap">{cartDescription}</p>
+				<p className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</p>
+				<div className="row">
+					<div className="d-grid col-12">{showAddToCart(addToCart)}</div>
+					<div className="d-grid col-12">{showRemoveFromCart(removeFromCart)}</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 import { getProducts } from "./helper/coreapicalls.js";
+import Base from "./Base.js";
+import Card from "./Card.js";
+import "../styles.css";
 
 export default function Home() {
 	const [products, setProducts] = useState([]);
@@ -23,17 +26,17 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div>
+		<Base title="Home Page" description="Welcome to Tshirt store">
 			<h1>Home component</h1>
 			<div className="row">
 				{products.map((product, index) => {
 					return (
-						<div key={index}>
-							<h1>{product.name}</h1>
+						<div key={index} className="col-4 mb-4">
+							<Card product={product} />
 						</div>
 					);
 				})}
 			</div>
-		</div>
+		</Base>
 	);
 }
